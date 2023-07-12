0853c4a286b05f738bc3c63eb5e53defd3fadc12
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-07-12 13:19:51 +0100

    Create animals table

---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE animals (
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT NOT NULL,
+    neutered BOOL,
+    weight_kg DECIMAL(5,2) NOT NULL
+);
